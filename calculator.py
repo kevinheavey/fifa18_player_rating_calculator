7ee9144f308f2ed6ab5f536de86c7829cb8e0498
kevinheavey <heaveykevin2@gmail.com>
2017-10-22 15:20:18 +0100

    tidy up

--- calculator.py
+++ calculator.py
@@ -26,17 +26,17 @@ def process_raw_rating(rating, international_reputation, rep_subtraction, potent
     return capped_contrib
 
 
-def calculate_ratings(stats_dict):
+def calculate_ratings(attribute_dict):
     position_blueprint_dict = get_position_blueprint_dict()
     root_positions = position_blueprint_dict.keys()
-    international_rep = stats_dict['International reputation']
-    potential = stats_dict['Potential']
+    international_rep = attribute_dict['International reputation']
+    potential = attribute_dict['Potential']
     position_ratings = {}
     for position in root_positions:
         sub_dict = position_blueprint_dict[position]
         coefficient_dict = sub_dict['coefficients']
         rep_subtraction = sub_dict['reputation_subtraction']
-        raw_position_rating = sum(coefficient * stats_dict[stat] 
+        raw_position_rating = sum(coefficient * attribute_dict[stat]
                                   for stat, coefficient in coefficient_dict.items())
         position_ratings[position] = process_raw_rating(raw_position_rating, international_rep, 
                                                         rep_subtraction, potential)
