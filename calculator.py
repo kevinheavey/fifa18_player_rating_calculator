f5acd2bcc2800d2b40b70bcd5ece49ddc6799443
kevinheavey <heaveykevin2@gmail.com>
2017-10-22 14:41:29 +0100

    use pathlib

--- calculator.py
+++ calculator.py
@@ -1,26 +1,30 @@
 import json
-from os.path import join, dirname
+from pathlib import Path
 
 
 def get_position_blueprint_dict():
-    parent_dir = dirname(__file__)
-    filepath = join(parent_dir, 'resources', 'position_rating_blueprint.json')
+    filepath = Path(__file__).parent / 'resources' / 'position_rating_blueprint.json'
     with open(filepath, 'r') as fp:
-        return json.load(fp)
+        result = json.load(fp)
+    return result
 
-def add_reputation(value, international_reputation, subtraction):    
+
+def add_reputation(value, international_reputation, subtraction):
     value = round(value)
     increase = max(international_reputation - subtraction, 1)
     return round(value + increase)
+
 
 def check_potential(value, potential):
     """Value cannot be higher than potential"""
     return min(value, potential)
 
+
 def process_raw_rating(rating, international_reputation, rep_subtraction, potential):
     contrib_plus_reputation = add_reputation(rating, international_reputation, rep_subtraction)
     capped_contrib = check_potential(contrib_plus_reputation, potential)
     return capped_contrib
+
 
 def calculate_ratings(stats_dict):
     position_blueprint_dict = get_position_blueprint_dict()
